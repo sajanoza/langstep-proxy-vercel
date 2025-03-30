e11f3183873ed8b982b3645c51ed2fe95be91db4
sajanoza <sajanmaciej@gmail.com>
2025-03-30 14:59:49 +0200

    Update translate.js
    allow orygin dodane

--- api/translate.js
+++ api/translate.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // lub "https://projektninja.pl" dla konkretnego źródła
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Obsługa zapytania wstępnego (preflight)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
